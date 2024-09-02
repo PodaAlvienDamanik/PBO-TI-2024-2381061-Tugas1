8f5f6580126f56bffecaf079fa486b724589373b
Alvin Damanik <2381061@unai.edu>
2024-09-02 20:32:57 +0700

    commit

---
+++ SortingaArrays.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+public class SortingaArrays {
+    public static void main(String[] args) {
+        int[] arr = {5,6,7,8,5,7,1};
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
